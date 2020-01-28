69bf37a71d9f4d8ad078b25fcc45caedba8d9991
plyn85 <plyn99@gmail.com>
2020-01-28 19:08:46 +0000

    adding thr add_tasks.html page

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from os import path
 if path.exists("env.py"):
   import env 
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -15,9 +15,17 @@ app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 mongo = PyMongo(app)
 
 @app.route('/')
+def index():
+  return redirect(url_for("add_task"))
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
+
 
 
 if __name__ == '__main__':
