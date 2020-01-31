ca8bb2c1e01b034755bf5dc613d7a944cbf70325
plyn85 <plyn99@gmail.com>
2020-01-31 17:11:02 +0000

    changes to add task section

--- app.py
+++ app.py
@@ -1,31 +1,40 @@
-import os 
+from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
+from flask import Flask, render_template, redirect, request, url_for
+import os
 from os import path
 if path.exists("env.py"):
-  import env 
-from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+    import env
 
 app = Flask(__name__)
 
 
 app.config["MONGO_DBNAME"] = "task_manager"
-app.config["MONGO_URI"] = os.environ.get('MONGO_URI') 
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
+
 
 @app.route('/')
 def index():
   return redirect(url_for("add_task"))
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template("tasks.html",
+                           tasks=mongo.db.tasks.find())
 
 
 @app.route('/add_task')
 def add_task():
-    return render_template('addtask.html', categories=mongo.db.categories.find())
+    return render_template('addtask.html',
+                           categories=mongo.db.categories.find())
 
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 
 if __name__ == '__main__':
