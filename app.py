68024ba071e2edb6fa261eddbfc5cb854ef1fece
plyn85 <plyn99@gmail.com>
2020-02-06 15:27:36 +0000

    adding categories.html an edit_category.html

--- app.py
+++ app.py
@@ -19,27 +19,26 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
+# this aims back at the tasks html and page and the mongo db database
+# we use the find function with mongo db to fetch our tasks
 @app.route('/get_tasks')
 def get_tasks():
-    # this aims back at the tasks html and page and the mongo db database
-    # we use the find function with mongo db to fetch our tasks
     return render_template("tasks.html",
                            tasks=mongo.db.tasks.find())
 
-
-@app.route('/add_task')
 # this aims back at the addtask html and page and the mongo db database
 # we use the find function with mongo db to fetch our categories
+
+
+@app.route('/add_task')
 def add_task():
     return render_template('addtask.html',
                            categories=mongo.db.categories.find())
 
 
-@app.route('/insert_task', methods=['POST'])
 # Now, because we're submitting a form, and we're submitting using POST,
 # we must refer to the HTTP method that will be used to deliver the form data.
 # in this case its post the default is get
-def insert_task():
     # the first line will get the tasks collection from the mongo db database
     # the second line will submit infomation to a web location in the form of a request object
     #  inside that we say show me the form
@@ -47,24 +46,40 @@ def insert_task():
     #  any of the form fields that have data inside them will be submitted to the tasks collection in mongodb
     #  during thr project from validation shoul be used and in add tasks html section
     #  the last line will redirect to get/tasks function  above so we can veiw the new task in our mongo db collection
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
     tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
+
 # adding function to connect to the edit task button here
 # In order to do do this we need to retrive the task from the data base
 #  we want to find one particular task from our tasks collection
-
 # Where looking for a match the _id the key in mongo
 # We put the tasks_Id parameter in a format that exceptable to mongo
 # we use the mongo db find function to grab categories
 
 
-@app.route('/edit_task/<task_id>')
+# EDIT tasks (U in CRUD -> Update/Edit)
+@app.route("/edit_task/<task_id>")
 def edit_task(task_id):
     the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     all_categories = mongo.db.categories.find()
-    return render_template('edit_task.html', task=the_task,
-                           categories=all_categories)
+    return render_template("edit_task.html", task=the_task, categories=all_categories)
+
+
+@app.route('/get_categories')
+def get_categories():
+
+    return render_template('categories.html',  categories=mongo.db.categories.find())
+
+
+# EDIT categories (U in CRUD -> Update/Edit)
+@app.route("/edit_category/<category_id>")
+def edit_category(category_id):
+    return render_template("edit_category.html",
+                           category=mongo.db.categories.find_one({"_id": ObjectId(category_id)}))
 
 
 if __name__ == '__main__':
